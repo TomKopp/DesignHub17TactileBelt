a3f50ae7a38918209dbc2868b8e156a85e165f53
Tom Kopp <witzbould@live.com>
2018-04-04 19:46:16 +0200

    add input serial parser

--- src/Config.h
+++ src/Config.h
@@ -1,6 +1,8 @@
 namespace DesignHub
 {
-static const int TICKS_PER_SECOND = 50;
+static const int TICKS_PER_SECOND = 20;
 static const int DEBOUNCEDELAY = 1000 / TICKS_PER_SECOND; // In milliseconds
 static const int MAX_SKIPPEDFRAMES = 10;
+
+static const int FORCE_CHAR_BUFFER_SIZE = 3; // In byte
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@ TactileBelt *Belt;
 unsigned long nextTick = millis();
 int skippedFrames;
 
+String forceCharBuffer = "";
+std::list<int> motorForces;
+int motorId = 0;
+TactileSettings *settings = nullptr;
+bool isSettingsReady = false;
+
 /**
  * Setup
  */
@@ -18,6 +24,8 @@ void setup()
   Belt->addMotor(0, 15);
   Belt->addMotor(1, 16);
   Belt->addMotor(2, 17);
+
+  forceCharBuffer.reserve(FORCE_CHAR_BUFFER_SIZE);
 
   Serial.begin(57600);
 
@@ -32,17 +40,22 @@ void setup()
  */
 void loop()
 {
-  // read Serial
-  // 0=20,0,100&1=0,0,30&7=200,0,255
-
-  // split String
-  // parse String
-
   skippedFrames = 0;
   while (millis() > nextTick && skippedFrames < MAX_SKIPPEDFRAMES)
   {
     // new TactileSettings()
-    Belt->update(nullptr);
+    if (isSettingsReady)
+    {
+      Belt->update(settings);
+
+      settings = nullptr;
+      isSettingsReady = false;
+    }
+    else
+    {
+      Belt->update(nullptr);
+    }
+
 
     nextTick += DEBOUNCEDELAY;
     skippedFrames++;
@@ -53,10 +66,46 @@ void loop()
 
 /**
  * SerialEvent
+ *
+ * read Serial
+ * 0=20,0,100&1=0,0,30&7=200,0,255\r\n
  */
 void serialEvent()
 {
+  if (settings == nullptr)
+  {
+    settings = new TactileSettings();
+  }
+
   while (Serial.available())
   {
+    char inChar = (char)Serial.read();
+
+    switch (inChar)
+    {
+    case ',': // push_back()
+    case '&':
+      settings->appendForce(motorId, forceCharBuffer.toInt());
+      forceCharBuffer = "";
+      break;
+    case '=': // buffer has motorId -> set _motorId
+      motorId = forceCharBuffer.toInt();
+      forceCharBuffer = "";
+      break;
+    case '\r':
+    case '\n':
+      // send TactileSettings
+      isSettingsReady = true;
+
+      // clear rest buffer
+      if (Serial.peek() == '\n')
+      {
+        Serial.read();
+      }
+      forceCharBuffer = "";
+      break;
+    default:
+      forceCharBuffer += inChar;
+    }
   }
 }
