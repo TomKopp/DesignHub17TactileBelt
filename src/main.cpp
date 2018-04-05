c888f26beb121751d4ff58a4f27814d62eb4a1e0
Tom Kopp <witzbould@live.com>
2018-04-05 18:45:53 +0200

    reorder function declaration/definition correct parsing switch add propper motors for hardware prototype add function to establish contact update game loop

--- src/main.cpp
+++ src/main.cpp
@@ -9,66 +9,27 @@ TactileBelt *Belt;
 unsigned long nextTick;
 int skippedFrames;
 
-// String forceCharBuffer = "";
-// std::list<int> motorForces;
-// int motorId = 0;
-// TactileSettings *settings = nullptr;
-// bool isSettingsReady = false;
+String forceCharBuffer = "";
+int motorId = 0;
+TactileSettings *settings = nullptr;
+bool isSettingsReady = false;
 
 /**
- * Setup
+ * Establish Connection
  */
-void setup()
+void establishContact()
 {
-  Belt = new TactileBelt();
-  Belt->addMotor(0, 12);
-  Belt->setMotorForce(0, 100);
-  // Belt->addMotor(1, 11);
-  // Belt->addMotor(2, 10);
-
-  // forceCharBuffer.reserve(FORCE_CHAR_BUFFER_SIZE);
-
   Serial.begin(57600);
-
   while (!Serial)
     ;
 
-  Serial.println("You're connected.");
-
-  // settings = new TactileSettings();
-  // settings->appendForce(0, 255);
-  // isSettingsReady = false;
-  nextTick = millis();
-}
-
-/**
- * Loop
- */
-void loop()
-{
-  skippedFrames = 0;
-  while (millis() > nextTick && skippedFrames < MAX_SKIPPEDFRAMES)
+  while (Serial.available() <= 0)
   {
-  //   // new TactileSettings()
-  //   if (isSettingsReady)
-  //   {
-  //     Belt->update(settings);
-
-  //     settings = nullptr;
-  //     isSettingsReady = false;
-  //   }
-  //   else
-  //   {
-  //     Belt->update(nullptr);
-  //   }
-
-  Serial.println("Tick");
-
-  nextTick += DEBOUNCEDELAY;
-  skippedFrames++;
+    // send a capital A
+    Serial.print('A');
+    delay(300);
   }
-
-  Belt->render();
+  Serial.println("\nYou're connected.");
 }
 
 /**
@@ -77,42 +38,88 @@ void loop()
  * read Serial
  * 0=20,0,100&1=0,0,30&7=200,0,255\r\n
  */
-// void serialEvent()
-// {
-//   if (settings == nullptr)
-//   {
-//     settings = new TactileSettings();
-//   }
+void mySerialEvent()
+{
+  if (settings == nullptr)
+  {
+    settings = new TactileSettings();
+  }
 
-//   while (Serial.available())
-//   {
-//     char inChar = (char)Serial.read();
+  while (Serial.available())
+  {
+    char inChar = (char)Serial.read();
 
-//     switch (inChar)
-//     {
-//     case ',': // push_back()
-//     case '&':
-//       settings->appendForce(motorId, forceCharBuffer.toInt());
-//       forceCharBuffer = "";
-//       break;
-//     case '=': // buffer has motorId -> set _motorId
-//       motorId = forceCharBuffer.toInt();
-//       forceCharBuffer = "";
-//       break;
-//     case '\r':
-//     case '\n':
-//       // send TactileSettings
-//       isSettingsReady = true;
+    switch (inChar)
+    {
+    case '=':
+      // buffer has motorId -> set _motorId
+      motorId = forceCharBuffer.toInt();
+      forceCharBuffer = "";
+      break;
+    case '\r':
+    case '\n':
+      // send TactileSettings
+      isSettingsReady = true;
 
-//       // clear rest buffer
-//       if (Serial.peek() == '\n')
-//       {
-//         Serial.read();
-//       }
-//       forceCharBuffer = "";
-//       break;
-//     default:
-//       forceCharBuffer += inChar;
-//     }
-//   }
-// }
+      // clear rest buffer
+      if (Serial.peek() == '\n')
+      {
+        Serial.read();
+      }
+      // expicit fall through
+    case ',':
+    case '&':
+      // buffer has next force as string
+      settings->appendForce(motorId, forceCharBuffer.toInt());
+      forceCharBuffer = "";
+      break;
+    default:
+      forceCharBuffer += inChar;
+    }
+  }
+}
+
+/**
+ * Setup
+ */
+void setup()
+{
+  establishContact();
+
+  Belt = new TactileBelt();
+  Belt->addMotor(0, 15);
+  Belt->addMotor(1, 16);
+  Belt->addMotor(2, 17);
+  Belt->addMotor(3, 18);
+  Belt->addMotor(4, 12);
+  Belt->addMotor(5, 11);
+  Belt->addMotor(6, 10);
+  Belt->addMotor(7, 9);
+
+  forceCharBuffer.reserve(FORCE_CHAR_BUFFER_SIZE);
+
+  nextTick = millis();
+}
+
+/**
+ * Loop
+ */
+void loop()
+{
+  mySerialEvent();
+  skippedFrames = 0;
+  while (millis() > nextTick && skippedFrames < MAX_SKIPPEDFRAMES)
+  {
+    if (isSettingsReady)
+    {
+      Belt->setSettings(settings);
+      isSettingsReady = false;
+      settings = nullptr;
+    }
+    Belt->update();
+    nextTick += DEBOUNCEDELAY;
+    skippedFrames++;
+  }
+
+  Belt->render();
+}
